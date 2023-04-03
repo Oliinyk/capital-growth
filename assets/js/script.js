e7c2d2e91802190655485a966172189a272c6108
oliinykroman <oliinykroman@gmail.com>
2023-04-03 22:12:00 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,6 @@ var modal = document.getElementById("myModal");
 var btn1 = document.getElementById("openModalJs");
 var btn2 = document.getElementById("openModalJs2");
 var closeBtn = document.getElementById("closeModalJs");
-
-
  
 // Function to open the modal 
 function openModal() {
@@ -42,40 +40,3 @@ window.onclick = function(event) {
     closeModal(); 
   } 
 }
-
-
-
-
-
-
-
-
-$('.modal-body .btn').click( function(e) {
-  e.preventDefault();
-
-  $(this).closest('#mc-embedded-subscribe-form').find('input.required').each(function() {
-    if ($(this).val() == "") {
-      e.preventDefault();
-
-      console.log('Error 2')
-    }
-  })
-
-  // || $(this).closest('#mc-embedded-subscribe-form').find('input.required').val().length === 0
-
-
-  if ($(this).closest('#mc-embedded-subscribe-form').find('input.mce_inline_error').length) {
-    console.log("Error 1")
-  } else {
-    $('#mc-embedded-subscribe-form').submit();
-    
-    window.setTimeout( function(){
-      location.href = 'https://oliinyk.github.io/capital-growth/conversion-magic.html'
-    }, 500 );
-
-  }
-})
-
-
-
-
